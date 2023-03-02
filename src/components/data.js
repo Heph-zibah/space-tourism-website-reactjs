aaca908aec2053d0712e32f167ae25b8af4c2c58
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-02 14:38:33 +0100

    created a data file

---
+++ src/components/data.js
@@ -0,0 +1,26 @@
+export const navLinks = [
+    {
+        id:1,
+        num: 00,
+        url:'/home',
+        link: 'home'
+    },
+    {
+        id:1,
+        num:01,
+        url:'/destination',
+        link: 'destination'
+    },
+    {
+        id:1,
+        num:02,
+        url:'/crew',
+        link: 'crew'
+    },
+    {
+        id:1,
+        num: 03,
+        url:'/technology',
+        link: 'technology'
+    },
+]

214d3fe13b76cca0cbbf94890aa7e458e0ed9612
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-02 16:09:41 +0100

    update data file

--- src/components/data.js
+++ src/components/data.js
@@ -1,25 +1,25 @@
 export const navLinks = [
     {
         id:1,
-        num: 00,
-        url:'/home',
+        num: '00',
+        url:'/',
         link: 'home'
     },
     {
         id:1,
-        num:01,
+        num:'01',
         url:'/destination',
         link: 'destination'
     },
     {
         id:1,
-        num:02,
+        num:'02',
         url:'/crew',
         link: 'crew'
     },
     {
         id:1,
-        num: 03,
+        num: '03',
         url:'/technology',
         link: 'technology'
     },

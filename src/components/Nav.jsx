75ef10a6ddee89c9ac7b4f7eb4bcb0ea57bb95f9
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-03 02:14:10 +0100

    revamp nav file

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,40 +1,55 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, { useState } from 'react'
+import {NavLink, Link } from 'react-router-dom'
 import { navLinks } from './data'
 import logo from '../starter-code/assets/shared/logo.svg'
 import menuOpen from '../starter-code/assets/shared/icon-hamburger.svg'  
 import menuClose from '../starter-code/assets/shared/icon-close.svg'
 
 const Nav = () => {
+    const [open, setOpen] = useState(false);
+    const [active, setActive] = useState(null);
   return (
    <>
-    <nav>
-        <img src={logo} alt="space tourism logo" />
-        <div className="nav__line"></div>
-        <ul>
-            {navLinks.map((item) => {
-                const {id, num, url,link} = item
-                return <NavLink to={url} key={id} activeClassName="active">
-                    <p>{num}
-                    {link}</p>
-                </NavLink>
-            })}
-        </ul>
-        <img src={menuOpen} alt="hamburger" />
-    </nav>
-    <div className="mobile__nav">
-        <img src={menuClose} alt="close icon" />
-        <ul>
-            {navLinks.map((item) => {
-                const {id, num, url,link} = item
-                return <NavLink to={url} key={id} activeClassName="active">
-                    <p>{num}
-                    {link}</p>
-                </NavLink>
-            })}
-        </ul>
-    </div>
-   </>
+      <header>
+      <div className="nav__logo">
+                <img src={logo} alt="space tourism logo" />
+            </div>
+      <nav>
+            
+            <span className="nav__line"></span>
+            <ul>
+                {navLinks.map((item) => {
+                    const {id, num, url,link} = item
+                    return <li key={id} onClick={() => setActive(item)} className={`list-item ${active == item && "active"}`}>
+                        <Link to={url}>
+                        <span>{num}</span>{link}
+                        </Link>
+                    </li>
+                    })}
+            </ul>
+            <div className="menu__open" onClick={() => setOpen(!open)}>
+                <img src={menuOpen} alt="hamburger" />
+            </div>
+        </nav>
+        {open && <div className="mobile__nav">
+            <div className='mobile__div'>
+                <div className="menu__close" onClick={() => setOpen(!open)}>
+                    <img src={menuClose} alt="close icon" />
+                </div>
+                <ul>
+                    {navLinks.map((item) => {
+                        const {id, num, url,link} = item
+                        return <li >
+                                <NavLink to={url} key={id} activeClassName="active" onClick={() => setOpen(!open)}>
+                                    <span>{num}</span>{link}
+                                </NavLink>
+                            </li>
+                        })}
+                </ul>
+            </div>
+        </div>} 
+      </header>
+    </>
   )
 }
 

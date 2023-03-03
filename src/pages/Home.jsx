5e9bfbc9703841953ddf24ee231b3ed3069a1e18
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-03 02:14:39 +0100

    inputed the tags for homee page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <main className='home__container'>
+        <div className='home__wrapper'>
+          <div className='home-info'>
+            <h3>So, you want to travel to</h3>
+            <h1> Space</h1>
+            <p>Let’s face it; if you want to go to space, you might as well genuinely go to 
+            outer space and not hover kind of on the edge of it. Well sit back, and relax 
+            because we’ll give you a truly out of this world experience!</p>
+          </div>
+          <div className='home__btn'>
+            <button>Explore</button>
+          </div>
+        </div>
+      </main>
+    </>
   )
 }
 

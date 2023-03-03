4e827dea01f4b2a00c7ebb296d7800ad38713bf2
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-03 10:29:27 +0100

    added a class to the button div

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <>
       <main className='home__container container'>
         <div className='home__wrapper'>
-          <div className='home-info'>
+          <div className='home__info'>
             <h3>So, you want to travel to</h3>
             <h1> Space</h1>
             <p>Let’s face it; if you want to go to space, you might as well genuinely go to 
             outer space and not hover kind of on the edge of it. Well sit back, and relax 
             because we’ll give you a truly out of this world experience!</p>
           </div>
-          <div className='home__btn'>
-            <button>Explore</button>
-          </div>
+          
+           <div className='home__btn--div'>
+            <Link className='home__btn' to='/destination'>explore</Link>
+           </div>
+        
         </div>
       </main>
     </>

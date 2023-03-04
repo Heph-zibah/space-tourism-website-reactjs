1572d4c98310bf92dafae275ddf917cf4088bcc6
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-04 21:25:45 +0100

    update the marku

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -2,27 +2,74 @@ import React, { useState } from 'react'
 import moonImage  from '../starter-code/assets/destination/image-moon.png'
 import marsImage  from '../starter-code/assets/destination/image-mars.png'
 import europaImage  from '../starter-code/assets/destination/image-europa.png'
-import titanImmage  from '../starter-code/assets/destination/image-titan.png'
+import titanImage  from '../starter-code/assets/destination/image-titan.png'
+
 const data = require('../starter-code/data.json')
+
 const Destination = () => {
   const [active, setActive] = useState(null);
   const [image, setImage] = useState(moonImage)
-  const [planet, setPlanet] = useState(data.destinations)
+  const [planet, setPlanet] = useState(data.destinations[0])
+
+  function handleClick(e, index) {
+    e.preventDefault();
+    setPlanet(data.destinations[index]);
+    setActive(index);
+
+    switch(data.destinations[index].name) {
+        case 'Moon':
+            setImage(moonImage);
+            break;
+        case 'Mars':
+            setImage(marsImage);
+            break;
+        case 'Europa':
+            setImage(europaImage);
+            break;
+        case 'Titan':
+            setImage(titanImage);
+            break;
+        default:
+            break;
+    }
+}
+
   return (
     <>
        <main className='destination__container container'>
         <h3><span>01</span> Pick your destination</h3>
         <div className="destination__wrapper">
-          <img src={image} alt="planet" />
+          <div className='destination__img'>
+            <img src={image} alt="planet" />
+          </div>
           <div className="destination__content">
             <ul>
-              {data.destinations.map((link, planet, index) => {
-                const {name} = link
-                return <li key={index}>
-                    {name}
+              {data.destinations.map(( planet, index) => {
+                
+                return <li key={index} className={`list-item ${active === index && "active"}`}>
+                  <button  onClick={(e) => handleClick(e, index)}>
+                    {planet.name}
+                </button>
                 </li>
               })}
             </ul>
+            <div className="destination__info">
+              <div className="destination__desc">
+                <h1>{planet.name}</h1>
+                <p>{planet.description}</p>
+              </div>
+              <div className="destination__stats">
+                <div>
+                  <p>Avg. distance</p>
+                  <h4>{planet.distance}</h4>
+                </div>
+                <div>
+                  <p>Est. travel time</p>
+                  <h4>{planet.travel}</h4>
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
        </main>

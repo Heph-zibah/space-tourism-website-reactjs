34900978ef768b30dc5f91090d193bdef08cc046
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-04 23:54:34 +0100

    upddate markup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,8 @@ const Home = () => {
         <div className='home__wrapper'>
           <div className='home__info'>
             <h3>So, you want to travel to</h3>
-            <h1> <Typewriter
+            <h1> 
+              <Typewriter
                 onInit={(typewriter)=> {
                 typewriter
                 .typeString("space") 
@@ -17,16 +18,17 @@ const Home = () => {
                 .start();
                 }}
                 />
-  </h1>
+            </h1>
             <p>Let’s face it; if you want to go to space, you might as well genuinely go to 
-            outer space and not hover kind of on the edge of it. Well sit back, and relax 
-            because we’ll give you a truly out of this world experience!</p>
+              outer space and not hover kind of on the edge of it. Well sit back, and relax 
+              because we’ll give you a truly out of this world experience!
+            </p>
           </div>
           
-           <div className='home__btn--div'>
+          {/* explore button */}
+          <div className='home__btn--div'>
             <Link className='home__btn' to='/destination'>explore</Link>
-           </div>
-        
+          </div>
         </div>
       </main>
     </>

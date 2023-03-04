5a3574fa45cb166e6977023408178ada8dfd3267
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-04 23:52:48 +0100

    added comment

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -6,48 +6,55 @@ import menuOpen from '../starter-code/assets/shared/icon-hamburger.svg'
 import menuClose from '../starter-code/assets/shared/icon-close.svg'
 
 const Nav = () => {
+    // state for opening nav and active link
     const [open, setOpen] = useState(false);
     const [active, setActive] = useState(null);
   return (
    <>
       <header>
-      <div className="nav__logo">
-                <img src={logo} alt="space tourism logo" />
+            {/* logo */}
+            <div className="nav__logo">
+                    <img src={logo} alt="space tourism logo" />
             </div>
-      <nav>
-            
-            <span className="nav__line"></span>
-            <ul>
-                {navLinks.map((item) => {
-                    const {id, num, url,link} = item
-                    return <li key={id} onClick={() => setActive(item)} className={`list-item ${active === item && "active"}`}>
-                        <Link to={url}>
-                        <span>{num}</span>{link}
-                        </Link>
-                    </li>
-                    })}
-            </ul>
-            <div className="menu__open" onClick={() => setOpen(!open)}>
-                <img src={menuOpen} alt="hamburger" />
-            </div>
-        </nav>
-        {open && <div className="mobile__nav">
-            <div className='mobile__div'>
-                <div className="menu__close" onClick={() => setOpen(!open)}>
-                    <img src={menuClose} alt="close icon" />
-                </div>
+
+            {/* Links */}
+            <nav>
+                <span className="nav__line"></span>
                 <ul>
                     {navLinks.map((item) => {
                         const {id, num, url,link} = item
-                        return <li >
-                                <NavLink to={url} key={id} activeClassName="active" onClick={() => setOpen(!open)}>
-                                    <span>{num}</span>{link}
-                                </NavLink>
-                            </li>
+                        return <li key={id} onClick={() => setActive(item)} className={`list-item ${active === item && "active"}`}>
+                            <Link to={url}>
+                            <span>{num}</span>{link}
+                            </Link>
+                        </li>
                         })}
                 </ul>
-            </div>
-        </div>} 
+
+                {/* Open menu */}
+                <div className="menu__open" onClick={() => setOpen(!open)}>
+                    <img src={menuOpen} alt="hamburger" />
+                </div>
+            </nav>
+
+            {/* Mobile nav */}
+            {open && <div className="mobile__nav">
+                <div className='mobile__div'>
+                    <div className="menu__close" onClick={() => setOpen(!open)}>
+                        <img src={menuClose} alt="close icon" />
+                    </div>
+                    <ul>
+                        {navLinks.map((item) => {
+                            const {id, num, url,link} = item
+                            return <li >
+                                    <NavLink to={url} key={id} activeClassName="active" onClick={() => setOpen(!open)}>
+                                        <span>{num}</span>{link}
+                                    </NavLink>
+                                </li>
+                            })}
+                    </ul>
+                </div>
+            </div>} 
       </header>
     </>
   )

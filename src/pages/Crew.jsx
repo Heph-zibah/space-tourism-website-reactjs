1fb3cb6c272490625037a49f8f76f0f97f64ed4a
oaDaramola <oluwatosinhephzibah@gmail.com>
2023-03-07 00:14:06 +0100

    added comments

--- src/pages/Crew.jsx
+++ src/pages/Crew.jsx
@@ -7,17 +7,18 @@ import crew4 from '../starter-code/assets/crew/image-anousheh-ansari.webp'
 const data = require('../starter-code/data.json')
 
 const Crew = () => {
-
+  // state for active, immages, and crew
   const [active, setActive] = useState(null)
   const [image, setImage] = useState(crew1)
   const [isCrew, setIsCrew] = useState(data.crew[0])
 
+  // function to handle the button click
   function handleClick(e, index) {
     e.preventDefault()
     setIsCrew(data.crew[index])
     setActive(index);
     
-
+    // switch case for the images
     switch(index) {
       case 0:
         setImage(crew1)
@@ -42,11 +43,14 @@ const Crew = () => {
         <h3><span>02</span> Meet your crew</h3>
         <div className="crew__wrapper">
 
+          {/* for the images */}
           <div className="crew__img">
             <img src={image} alt="avatar" />
           </div>
 
+          {/* the crew ccontent */}
           <div className="crew__content">
+            {/* clickable buttons: mobile*/}
             <div className="crew__links mobile">
               {data.crew.map( (isCrew, index) => {
                 return <button key={index} className={`crew__link ${active === index && "active"}`} 
@@ -54,10 +58,13 @@ const Crew = () => {
               })}
             </div>
 
+              {/* crew description */}
               <div className="crew__desc">
                 <h6>{isCrew.role}</h6>
                 <h2>{isCrew.name}</h2>
                 <p>{isCrew.bio}</p>
+                
+                {/* clickable description:desktop */}
                 <div className="crew__links desktop">
               {data.crew.map( (isCrew, index) => {
                 return <button key={index} className={`crew__link ${active === index && "active"}`} 
